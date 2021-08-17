07982cb27015be4a399325a33c2810b781f71ad6
Akash S <akashsureshmyd@gmail.com>
2021-08-17 21:22:02 +0530

    just do it

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index():
 
 @app.route('/favicon.ico') 
 def favicon(): 
-    return os.send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return ""
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
--- assets/create_table.py
+++ assets/create_table.py
@@ -1,7 +1,7 @@
 import mysql.connector as msc
 
 def create_table():
-    mydb = msc.connect(host="sql6.freemysqlhosting.net", user="sql6429073", passwd='8uMGp8HslA', database='sql6429073')
+    mydb = msc.connect(host="db4free.net", user="akku1234", passwd='akku4321', database='testakku1234')
     mycursor = mydb.cursor()
     sql = "CREATE TABLE if not exists people(s_id int(5) PRIMARY KEY,name varchar(25),email varchar(20),password varchar(10),date_of_birth date,gender varchar(1));"
     mycursor.execute(sql)
